a7379e9be947c453eff497a61eef56b60e7b0243
Logan Franken <loganfranken@gmail.com>
2021-05-01 11:41:27 -0700

    Added basic React set-up

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+import path from 'path';
+import 'webpack';
+
+export default {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.tsx'
+  ],
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+      '.tsx',
+      '.ts'
+    ],
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    }
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
